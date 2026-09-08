f0e13f26528e749aa5db76e8042e731999019399
Joao <joao@gmail.com>
2026-09-08 17:53:40 -0300

    Atualização do projeto

--- src/Pointer.java
+++ src/Pointer.java
@@ -1,5 +1,44 @@
 package src;
 
 public class Pointer {
+    private int id;
+    private String nome;
+    private boolean disponibilidade;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+    public boolean getDisponibilidade() {
+        return disponibilidade;
+    }
+
+
+
+
+    
+
+
+    
+
+    
+
+
+
+
+
 
 }
